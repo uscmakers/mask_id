4f639540b0b851d41b7592e5c51754b10d20bb20
Radhika Agrawal <agrawalr@usc.edu>
2020-09-20 22:46:18 -0700

    initial servo test

---
+++ servo_test/servo_test.ino
@@ -0,0 +1,25 @@
+#include <Servo.h>
+
+Servo servo;
+int angle = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  servo.attach(7);
+  servo.write(angle);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(angle = 10; angle < 180; angle++)  
+  {                                  
+    servo.write(angle);               
+    delay(15);                   
+  } 
+  // now scan back from 180 to 0 degrees
+  for(angle = 180; angle > 10; angle--)    
+  {                                
+    servo.write(angle);           
+    delay(15);       
+  }
+}
