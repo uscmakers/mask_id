9a3eab0ae5a239565f8c70d73cf310205974d85e
Reshma Kopparapu <kopparap@usc.edu>
2020-10-04 11:38:33 -0700

    added reed switch

---
+++ reed_switch/reed_switch.ino
@@ -0,0 +1,25 @@
+//------------------------------------//
+//             Created by             //
+//         Alessio Bigini 2015        //
+//       http://alessiobigini.it      //
+//------------------------------------//
+const int pinSwitch = 12;  //Pin Reed
+const int pinLed    = 9;  //Pin LED
+int StatoSwitch = 0;
+void setup()
+{
+  pinMode(pinLed, OUTPUT);      //Imposto i PIN
+  pinMode(pinSwitch, INPUT);
+}
+void loop()
+{
+  StatoSwitch = digitalRead(pinSwitch);  //Leggo il valore del Reed
+  if (StatoSwitch == HIGH)
+  {
+    digitalWrite(pinLed, HIGH);
+  }
+  else
+  {
+    digitalWrite(pinLed, LOW);
+  }
+}
