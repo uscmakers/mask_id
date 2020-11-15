2c9379d9bb85a1e13025a1453828b10072e4701b
thejonmonte <jmontema@usc.edu>
2020-11-15 01:14:05 -0800

    indentation

--- pi_video.py
+++ pi_video.py
@@ -32,8 +32,8 @@ class VideoGet:
 
     def __init__(self, src=0):
         self.stream = cv2.VideoCapture(src)
-    	(self.grabbed, self.frame) = self.stream.read()
-		self.frame = imutils.resize(self.frame, width=400)
+        (self.grabbed, self.frame) = self.stream.read()
+        self.frame = imutils.resize(self.frame, width=400)
         self.stopped = False
 
     def start(self):    
@@ -46,7 +46,7 @@ class VideoGet:
                 self.stop()
             else:
                 (self.grabbed, self.frame) = self.stream.read()
-				self.frame = imutils.resize(self.frame, width=400)
+                self.frame = imutils.resize(self.frame, width=400)
 
     def stop(self):
         self.stopped = True
