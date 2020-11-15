385d17219e03f84b383b37e90a729d01c2bbb97d
thejonmonte <jmontema@usc.edu>
2020-11-15 02:25:37 -0800

    test model changes

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -132,15 +132,15 @@ ap.add_argument("-d", "--display", type=int, default=-1,
 args = vars(ap.parse_args())
 
 # # load our serialized face detector model from disk
-# print("[INFO] loading face detector model...")
-# prototxtPath = os.path.sep.join([args["face"], "deploy.prototxt"])
-# weightsPath = os.path.sep.join([args["face"],
-#     "res10_300x300_ssd_iter_140000.caffemodel"])
-# faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+print("[INFO] loading face detector model...")
+prototxtPath = os.path.sep.join([args["face"], "deploy.prototxt"])
+weightsPath = os.path.sep.join([args["face"],
+    "res10_300x300_ssd_iter_140000.caffemodel"])
+faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
-# # # load the face mask detector model from disk
-# print("[INFO] loading face mask detector model...")
-# maskNet = load_model(args["model"])
+# # load the face mask detector model from disk
+print("[INFO] loading face mask detector model...")
+maskNet = load_model(args["model"])
 
 i = 0
 rpi_data = []
@@ -190,39 +190,39 @@ while True:
         allMask = True
         numfaces = []
 
-        # detect faces in the frame and determine if they are wearing a
-        # face mask or not
-        (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
-
-        # loop over the detected face locations and their corresponding
-        # locations
-        for (box, pred) in zip(locs, preds):
-            # unpack the bounding box and predictions
-            (startX, startY, endX, endY) = box
-            (mask, withoutMask) = pred
-
-            # determine the class label and color we'll use to draw
-            # the bounding box and text
-            label = "Mask" if mask > withoutMask else "No Mask"
-            color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
-
-            ######################### NEW
-            if label == "No Mask":
-                allMask = False
-                
-
-            # include the probability in the label
-            label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
-
-            # display the label and bounding box rectangle on the output
-            # frame
-            cv2.putText(frame, label, (startX, startY - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-
-        ####################################### NEW
-
         if i == 2:
+            # detect faces in the frame and determine if they are wearing a
+            # face mask or not
+            (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+
+            # loop over the detected face locations and their corresponding
+            # locations
+            for (box, pred) in zip(locs, preds):
+                # unpack the bounding box and predictions
+                (startX, startY, endX, endY) = box
+                (mask, withoutMask) = pred
+
+                # determine the class label and color we'll use to draw
+                # the bounding box and text
+                label = "Mask" if mask > withoutMask else "No Mask"
+                color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+                ######################### NEW
+                if label == "No Mask":
+                    allMask = False
+                    
+
+                # include the probability in the label
+                label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+
+                # display the label and bounding box rectangle on the output
+                # frame
+                cv2.putText(frame, label, (startX, startY - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+                cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+                cv2.imshow("Video", frame)
+
+            ####################################### NEW
             if allMask == True:
                 rpi_data.append(1)
                 maskcount = maskcount + 1
